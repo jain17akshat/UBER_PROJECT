f0ebf0bc6f75bcee42060985d72f308cb8e0d156
jain17akshat <jain.7akshat@gmail.com>
2025-05-03 15:25:43 +0530

    Refactor user context and login pages

--- UBER-FRONTEND/UBER_FRONTEND/src/pages/Home.jsx
+++ UBER-FRONTEND/UBER_FRONTEND/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import {Link} from'react-router-dom';
 const Home=()=>{
     return(
         <div>
-        <div className='h-screen  pt-8 flex justify-between flex-col w-full bg-red-400'>
-        <img   className="w-14 ml-8" src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+<div className='bg-cover bg-center bg-[url("https://images.unsplash.com/photo-1619059558110-c45be64b73ae?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")] h-screen pt-8 flex justify-between flex-col w-full'>
+<img   className="w-14 ml-8" src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
 
-                <div className="bg-white py-5 px-10">
+                <div className="bg-white pb-7  py-4 px-4">
                     <h2 className="text-2xl font-bold">Get Started with Uber</h2>
-                    <button className="w-full bg-black text-white py-3 rounded-2 ">Continue</button>
+                    <Link to= '/login' className=" flex  items-center justify-center w-full bg-black text-white py-3 rounded- mt-4 ">Continue</Link>
                 </div>
             </div>
         </div>
