255197a18c01b3f80b63836bf0beee879a29eb46
jain17akshat <jain.7akshat@gmail.com>
2025-05-09 15:43:50 +0530

    Refactor CaptainDetails component and add CaptainHome page

--- UBER-FRONTEND/UBER_FRONTEND/src/pages/CaptainHome.jsx
+++ UBER-FRONTEND/UBER_FRONTEND/src/pages/CaptainHome.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+import CaptainDetails from '../components/CaptainDetails'
+
+import RidePopUp from '../components/RidePopUp'
+import ConfirmRidePopUp from '../components/ConfirmRidePopUp'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const CaptainHome = () => {
-  return (
-    <div>CaptainHome</div>
-  )
+    const [ridePopupPanel, setRidePopupPanel] = useState(true)
+    const [confirmRidePopupPanel, setConfirmRidePopupPanel] = useState(false)
+
+    const ridePopupPanelRef = useRef(null)
+    const confirmRidePopupPanelRef = useRef(null)
+
+    useGSAP(() => {
+        gsap.to(ridePopupPanelRef.current, {
+            transform: ridePopupPanel ? 'translateY(0)' : 'translateY(100%)',
+        })
+    }, [ridePopupPanel])
+
+    useGSAP(() => {
+        gsap.to(confirmRidePopupPanelRef.current, {
+            transform: confirmRidePopupPanel ? 'translateY(0)' : 'translateY(100%)',
+        })
+    }, [confirmRidePopupPanel])
+
+    return (
+        <div>
+            <div className='h-screen'>
+                <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
+                    <img className='w-16' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="Uber Logo" />
+                    <Link to='/captain-home' className='h-10 w-10 bg-white flex items-center justify-center rounded-full'>
+                        <i className="text-lg font-medium ri-logout-box-r-line"></i>
+                    </Link>
+                </div>
+                <div className='h-3/5'>
+                    <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="Car animation" />
+                </div>
+                <div className='h-2/5 p-6'>
+                    <CaptainDetails />
+                </div>
+                <div ref={ridePopupPanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+                    <RidePopUp setRidePopupPanel={setRidePopupPanel} setConfirmRidePopupPanel={setConfirmRidePopupPanel} />
+                </div>
+                <div ref={confirmRidePopupPanelRef} className='fixed w-full h-screen z-10 bottom-0 translate-y-full bg-white px-3 py-10 pt-12'>
+                    <ConfirmRidePopUp setConfirmRidePopupPanel={setConfirmRidePopupPanel} setRidePopupPanel={setRidePopupPanel} />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default CaptainHome
