be16867fcb82c0aff35dee19fa03771e826b2c53
jain17akshat <jain.7akshat@gmail.com>
2025-06-03 13:49:59 +0530

    Add map-related routes and controller for handling coordinates, distance, and suggestions

--- UBER-BACKEND/app.js
+++ UBER-BACKEND/app.js
@@ -6,7 +6,7 @@ const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const cookieParser=require("cookie-parser")
 const captainRoutes=require("./routes/captain.routes")
-
+const mapsRoutes =require('./routes/maps.routes')
 dotenv.config();
 
 // Connect to the database after the environment variables are loaded
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
  app.use(cookieParser())
-
+app.use('/maps',mapsRoutes);
 
 
 
