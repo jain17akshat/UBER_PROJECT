51610176fbf3e25b0524522140f5b30811bbafa6
jain17akshat <jain.7akshat@gmail.com>
2025-06-05 11:47:43 +0530

    Add Socket.IO integration for real-time communication and update ride controller

--- UBER-BACKEND/app.js
+++ UBER-BACKEND/app.js
@@ -1,39 +1,34 @@
-const express = require('express');
-const app = express();
 const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
 const cors = require('cors');
+const app = express();
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
-const cookieParser=require("cookie-parser")
-const captainRoutes=require("./routes/captain.routes")
-const mapsRoutes =require('./routes/maps.routes')
-const rideRoutes=require("./routes/ride.routes")
+const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
 
-
-
-dotenv.config();
-
-// Connect to the database after the environment variables are loaded
 connectToDb();
-
-
-
-
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
- app.use(cookieParser())
-app.use('/maps',mapsRoutes);
-app.use('/rides',rideRoutes);
-
+app.use(cookieParser());
 
 
 
 app.get('/', (req, res) => {
-  res.send("Hello world");
+    res.send('Hello World');
 });
 
 app.use('/users', userRoutes);
-app.use('/captains',captainRoutes)
+app.use('/captains', captainRoutes);
+app.use('/maps', mapsRoutes);
+app.use('/rides', rideRoutes);
+
+
+
+
 module.exports = app;
